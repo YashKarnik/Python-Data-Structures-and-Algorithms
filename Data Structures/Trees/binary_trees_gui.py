98f888e53efe7a8d041280e51b0438839c7070ee
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-02-26 01:21:34 +0530

    fix(bugs)

---
+++ Data Structures/Trees/binary_trees_gui.py
@@ -0,0 +1,9 @@
+# from binary_search_tree import BinarySearchTree
+import turtle
+if __name__ == '__main__':
+    # trt = turtle()
+    # turtle.color('green', 'red')
+    T = turtle.Turtle()
+    T.write("Home = ", True, align="left", font=("Arial", 18, "bold"))
+    T.ht()
+    turtle.done()
